82941cb4b20091147cdc829aff86dd46690361b3
Patrick <144135033+PatrickSchubert-Munich@users.noreply.github.com>
2024-02-24 13:00:13 +0100

    Add first time Project files
    first initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OBJC_WS2023_Hausarbeit_Schubert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
